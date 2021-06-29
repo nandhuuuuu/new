92a684159c25b1bc65a4cfb5591472d6e1a1bbc0
nandhuuuuu <86616957+nandhuuuuu@users.noreply.github.com>
2021-06-29 21:31:27 +0530

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,54 @@
+# #print("hi") 
+# # a=10
+# # b=10
+# # if a>b:
+# #     print("a is greater than b")
+# # elif a<b:
+#     # print("a is less than b")
+# # # else:
+# #     # print("a is equal to")
+# # # x=input() 
+# # a=100
+# # if a>20:
+# #     print("a is greater than 20")
+#     # if a>50:
+#         # print("a is greater than 50")
+
+
+# # a=10
+# # b=20
+# # c=30
+# # if a>b and a>c:
+#     # print("a is greater than b and c")
+# # else:
+#     # print("a is greater than c")
+
+# # a=10
+# # b=20
+# # c=30
+# # if a>c:
+#     # print("a is greater than c")
+# # else:
+#     # print("c is greater than a")
+
+
+# a=input("value of a\t")
+# b=input("value of b\n")
+# if a==b:
+#     print("a is equal to b")
+# else:
+#     print("a is not equal to b")
+    
+
+a=input("enter a value of a")
+a=int(a)
+if a>0 and a<10:
+    print("success")
+    if a%2==0:
+        print("a is even")
+    else:
+        print("a is odd")
+else:
+    print("fail")
+
+
